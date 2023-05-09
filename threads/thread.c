1cfaf24fa0301a8c3b06552f69443b51302cce02
e-juhee <juhee971204@gmail.com>
2023-05-09 06:11:38 +0000

    Project2: System Calls

--- threads/thread.c
+++ threads/thread.c
@@ -207,7 +207,7 @@ tid_t thread_create(const char *name, int priority, thread_func *function, void 
 	// 현재 스레드의 자식으로 추가
 	list_push_back(&thread_current()->child_list, &t->child_elem);
 
-	t->fdt = palloc_get_page(PAL_ZERO);
+	t->fdt = palloc_get_multiple(PAL_ZERO, FDT_PAGES);
 	if (t->fdt == NULL)
 		return TID_ERROR;
 	/* Add to run queue. */
